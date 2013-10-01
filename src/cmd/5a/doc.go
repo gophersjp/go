3596b85e9a30c6e11f20a22cabda6fb55410b859
ryochack <ryochack@gmail.com>
2013-10-02 02:35:03 +0900

    Add ARM gc docs

---
+++ src/cmd/5a/doc.go
@@ -0,0 +1,17 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build ignore
+
+/*
+
+5aはPlan 9用のアセンブラです。元のドキュメントについては以下を参照してください。
+
+        http://plan9.bell-labs.com/magic/man2html/1/8a
+
+ARMアーキテクチャをターゲットとしており、これらのツールにはarmとして参照されます。
+
+本ドキュメントは以下のドキュメントを翻訳しています: https://code.google.com/p/go/source/browse/src/cmd/5a/doc.go?r=3633a89bb56d
+*/
+package main
---
+++ src/cmd/5g/doc.go
@@ -0,0 +1,16 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build ignore
+
+/*
+
+5gはARM用のgcコンパイラです。
+このツールの$GOARCHはarmです。
+
+これは.goファイルを読み込み、.5ファイルを出力します。このコマンドのフラグについては ../gc/doc.go を参照してください。
+
+本ドキュメントは以下のドキュメントを翻訳しています: https://code.google.com/p/go/source/browse/src/cmd/5g/doc.go?r=3633a89bb56d
+*/
+package main
