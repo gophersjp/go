9cb665012729ec1dda34af23e7e345f11fb233b9
Ato Araki <ato.araki@gmail.com>
2014-03-12 21:48:21 +0900

    cmd/goimports: update doc, remove old gofmt flags
    refs
    https://code.google.com/p/go/source/browse/cmd/goimports/doc.go?repo=tools&r=8caf575e6beacc2237f7d78f8bd349c28f1b6b3e

--- src/pkg/code.google.com/p/go.tools/cmd/goimports/doc.go
+++ src/pkg/code.google.com/p/go.tools/cmd/goimports/doc.go
@@ -1,16 +1,18 @@
 /*
 
-goimportsコマンドはGoのimport節にある不足しているimportを追加したり、参照されていないimport文を削除します。
+goimportsコマンドは、Goのimport節で不足しているimportを自動で追加したり、参照されていないimport文を削除するgofmt互換のツールです。
 インストールは:
 
      $ go get code.google.com/p/go.tools/cmd/goimports
 
-このコマンドはgofmtコマンドのフォークですので、
-自分のエディタのgofmt-on-saveフックの代用として利用できます。
+です。
 
-emacsでは、(Go 1.2)の go-mode.elを利用します:
+goimportsは、自分のエディタのgofmt-on-saveフックの置き換えができます。
+goimportsは、gofmtのコマンドラインインタフェースと同じであり、同じ方法で
+コードのフォーマットができるようになっています。
+
+emacsでは、(Go 1.2+)の go-mode.elを利用します:
    https://go.googlecode.com/hg/misc/emacs/go-mode.el
-
 .emacsファイルに以下を追加しましょう:
    (setq gofmt-command "goimports")
    (add-to-list 'load-path "/home/you/goroot/misc/emacs/")
@@ -18,7 +20,6 @@ emacsでは、(Go 1.2)の go-mode.elを利用します:
    (add-hook 'before-save-hook 'gofmt-before-save)
 
 For vim, set "gofmt_command" to "goimports":
-
     https://code.google.com/p/go/source/detail?r=39c724dd7f252
     https://code.google.com/p/go/source/browse#hg%2Fmisc%2Fvim
     etc
@@ -30,9 +31,7 @@ For other editors, you probably know what to do.
 
 Happy hacking!
 
-本ドキュメントは以下のドキュメントを翻訳しています: https://code.google.com/p/go/source/browse/cmd/goimports/doc.go?repo=tools&r=7dd9cfdeec4318ebfb8db987fe898645fa024f5d
+本ドキュメントは以下のドキュメントを翻訳しています: https://code.google.com/p/go/source/browse/cmd/goimports/doc.go?repo=tools&r=8caf575e6beacc2237f7d78f8bd349c28f1b6b3e
 
 */
 package main
-
-import "code.google.com/p/go.tools/cmd/goimports"
