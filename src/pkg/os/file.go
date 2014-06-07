8f68fc45cfbf8a120a15f1aaa03897b577817300
Daigo UCHIYAMA <ucho@fuchidori.com>
2014-06-07 16:46:05 +0900

    translated file.go. #51

--- src/pkg/os/file.go
+++ src/pkg/os/file.go
@@ -2,30 +2,31 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package os provides a platform-independent interface to operating system
-// functionality. The design is Unix-like, although the error handling is
-// Go-like; failing calls return values of type error rather than error numbers.
-// Often, more information is available within the error. For example,
-// if a call that takes a file name fails, such as Open or Stat, the error
-// will include the failing file name when printed and will be of type
-// *PathError, which may be unpacked for more information.
+// osパッケージはOS機能へのプラットフォーム非依存なインターフェースを提供します。
+// 設計はUnixライクですが、エラーの扱いはGoライクになっており、
+// 関数呼び出しの失敗時にはエラー番号ではなく、error型の値を返します。
+// errorからはより多くの情報が得られることがあります。
+// 例えば、ファイル名を受け取る関数呼び出し(OpenやStatなど)が失敗したとき、
+// errorをprintすると失敗したファイル名が得られ、またerrorを*PathError型の値
+// として扱うことで、より詳細な情報が得られます。
 //
-// The os interface is intended to be uniform across all operating systems.
-// Features not generally available appear in the system-specific package syscall.
+// osパッケージは全てのOSに共通のインターフェースを提供することを目指しています。
+// 共通ではない機能はシステム依存なパッケージであるsyscallにあります。
 //
-// Here is a simple example, opening a file and reading some of it.
+// 以下はファイルを開いてそれを読む簡単な例です。
 //
 //	file, err := os.Open("file.go") // For read access.
 //	if err != nil {
 //		log.Fatal(err)
 //	}
 //
-// If the open fails, the error string will be self-explanatory, like
+// ファイルを開くのに失敗すると、errorは以下のようなエラーの内容を示す文字列
+// になります。
 //
 //	open file.go: no such file or directory
 //
-// The file's data can then be read into a slice of bytes. Read and
-// Write take their byte counts from the length of the argument slice.
+// ファイルのデータはbyteのスライスへ読み込まれます。
+// ReadとWriteは引数に渡されたスライスの長さをバイト数として返します。
 //
 //	data := make([]byte, 100)
 //	count, err := file.Read(data)
@@ -34,4 +35,5 @@
 //	}
 //	fmt.Printf("read %d bytes: %q\n", count, data[:count])
 //
+// 本ドキュメントは以下のドキュメントを翻訳しています: https://code.google.com/p/go/source/browse/src/pkg/os/file.go?r=5142686ded576fd930ba36b3aeac9b7c3a819d6b
 package os
